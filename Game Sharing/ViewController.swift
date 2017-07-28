6d3cf05ee413f75435dc5ef32cd4bb4407754d18
Spencer Cook <sgablec@uwo.ca>
2017-07-27 23:47:37 -0400

    Initial Commit

---
+++ Game Sharing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game Sharing
+//
+//  Created by Spencer Cook on 2017-07-27.
+//  Copyright © 2017 Spencer Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
